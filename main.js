38f201e9941c90c5e2243e9f19173d0bc8ef0bee
Nayan-Baishya <113695947+Nayan-Baishya@users.noreply.github.com>
2023-04-28 17:33:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+if(condition){
+    counter++
+}
+
+if(stroke=='black'||stroke=='red'){
+    line(30,40,25,50)
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas() {
+    background("white");
+}
